d95a8dff6c78cb17acc545ad64d13f1819e61bc7
Changjiang Liu <changjiangliu@Changjiangs-MBP.home>
2024-06-16 19:10:43 -0700

    setup deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -10,10 +11,11 @@ import connectToMongoDB from "./db/connectToMongoDB.js";
 import { app, server } from "./socket/socket.js";
 import passport from "passport";
 import "./routes/passport-setup.js";
-import cors from "cors";
+
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 // to parse the incoming requests with JSON payloads (from req.body)
 app.use(express.json({ limit: "10mb" }));
@@ -22,15 +24,15 @@ app.use(cookieParser());
 
 app.use(passport.initialize());
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    methods: "GET,POST,PUT,DELETE",
-    credentials: true
-}));
-
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, "frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 server.listen(PORT, () => {
     connectToMongoDB();
--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -5,12 +5,7 @@ import express from 'express';
 const app = express();
 
 const server = http.createServer(app);
-const io = new Server(server, {
-    cors: {
-        origin: ["http://localhost:5173"],
-        method: ["GET", "POST"]
-    }
-});
+const io = new Server(server);
 
 export const getReceiverSocketId = (receiverId) => {
     return userSocketMap[receiverId];
